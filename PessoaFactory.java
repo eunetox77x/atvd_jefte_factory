5c4e657ce2260596d8a97c6a60436694b9566bba
Miguel Neto <167142789+eunetox77x@users.noreply.github.com>
2025-04-23 19:29:44 -0300

    Atualizar o PessoaFactory.java

--- PessoaFactory.java
+++ PessoaFactory.java
@@ -1,3 +1,9 @@
 public class PessoaFactory {
-    
+    private int contador = 0;
+
+    public Pessoa criaPessoa(String nome, int idade) {
+        Pessoa pessoa = new Pessoa(contador, nome, idade);
+        contador++;
+        return pessoa;
+    }
 }
